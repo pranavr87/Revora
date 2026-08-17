e34f38b36bad21bdcc7eae2d74c82f660af2ec63
Pranav Raj <pranavraj2625@gmail.com>
2026-08-17 23:43:19 +0530

    Fix full width layout

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -15,6 +15,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
+        "https://revora-1.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
